56a3dd3fb391d7f48a8933e52fce9e4d2139114d
gustavomelo20 <infogustavomelo@gmail.com>
2025-03-10 22:45:33 -0300

    chore: product/category model

---
+++ APICatalogo/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace APICatalogo.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? imageUrl { get; set; }
+}
