76cfb99078a80019162669fcd2c7999e726324ae
gustavomelo20 <infogustavomelo@gmail.com>
2025-03-10 22:35:26 -0300

    chore: remove WeatherForecast

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4175349d3a6ce6a603f0ab89cc1bf854fdf8a942")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
